6cba3b366faf1be993d3c7aa607d1f8f9c17a681
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-04 04:43:16 -0400

    change circle color and give message at end

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 const circle = document.querySelector('circle');
+const timerDone = document.querySelector('p');
 
 //* Set perimeter and stroke-dasharray
 const perimeter = circle.getAttribute('r') * 2 * Math.PI;
 circle.setAttribute('stroke-dasharray', perimeter);
+
+const countDownColor = circle.getAttribute('stroke');
 
 let duration;
 //* New instance of Timer, fourth argument (optional callbacks)
 const timer = new Timer(durationInput, startButton, pauseButton, {
   //* Hook Timer to the real world
   onStart(totalDuration) {
+    timerDone.innerText = ""
     duration = totalDuration;
   },
   onTick(timeRemaining) {
@@ -20,8 +24,14 @@ const timer = new Timer(durationInput, startButton, pauseButton, {
       'stroke-dashoffset',
       (perimeter * timeRemaining) / duration - perimeter
     );
+
+    if (circle.getAttribute('stroke-dashoffset') < -perimeter / 2) {
+      circle.setAttribute('stroke', 'red');
+    } else {
+      circle.setAttribute('stroke', 'green');
+    }
   },
   onComplete() {
-    console.log('Timer is completed');
+    timerDone.innerText = 'Timer Is Done';
   },
 });
