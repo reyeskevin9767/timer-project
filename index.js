36cdb803fd501089bb259b805be79d8fa510f449
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-04 01:11:58 -0400

    create class timer

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hi There');
+class Timer {
+  //* Constructor takes DOM elements
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    //* Event Listeners
+    this.startButton.addEventListener('click', this.start);
+  }
+  start() {
+    console.log('Time to start the timer!');
+  }
+}
+
+//* Select DOM elements
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+//* Create a new instance of Timer
+const timer = new Timer(durationInput, startButton, pauseButton);
