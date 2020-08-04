c623a5ff5e55c2998bfefc606ce95d83f47451c8
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-04 03:12:01 -0400

    set perimeter and dash-array

--- index.js
+++ index.js
@@ -2,7 +2,13 @@
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
+//* Set perimeter and stroke-dasharray
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let currentOffset = 0;
 //* New instance of Timer, fourth argument (optional callbacks)
 const timer = new Timer(durationInput, startButton, pauseButton, {
   //* Hook Timer to the real world
@@ -10,7 +16,8 @@ const timer = new Timer(durationInput, startButton, pauseButton, {
     console.log('Timer Started');
   },
   onTick() {
-    console.log('Timer just ticked down');
+    circle.setAttribute('stroke-dashoffset', currentOffset);
+    currentOffset -= 50;
   },
   onComplete() {
     console.log('Timer is completed');
